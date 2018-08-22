7b24766c46205ce3c2508584d2f206f381f5d3cf
Edmond <chensje@hotmail.com>
2018-08-22 15:56:20 -0500

    init code

---
+++ code/model.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+class UserBasedCF(object):
+    def __init__(self):
+        pass
+
+    def fit():
+        '''
+        Trains CF model on train data. 
+
+        Parameters 
+        ----------
+        train_data : pd.DataFrame
+            with columns ['..','..']
+
+        Returns
+        ----------
+        top k predictions 
+        '''
+        preprocess()
+        find_similar_drivers()
+        find_k_preds()
+        test_preds()
+        get_score()
+        pass
+
+
+    def find_similar_drivers():
+        pass
+
+
+    def find_k_preds():
+        pass
