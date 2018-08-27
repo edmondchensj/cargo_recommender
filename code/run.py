cace9ce300079d0ea35e2e04f5317e347e1454fb
Edmond <chensje@hotmail.com>
2018-08-27 13:25:58 -0500

    completed first round of implicit CF

--- code/run.py
+++ code/run.py
@@ -1,37 +1,35 @@
-import implicit # see https://github.com/benfred/implicit
+from implicit.als import AlternatingLeastSquares # see https://github.com/benfred/implicit
 import pandas as pd
 import numpy as np
 import time
+import random
 import scipy.sparse as sparse
+import pickle
+from sklearn import metrics
 from utils import load_from_SQL
 
+# Blog explanations: 
+# https://towardsdatascience.com/recommending-github-repositories-with-google-bigquery-and-the-implicit-library-e6cce666c77
+# https://jessesw.com/Rec-System/
 def preprocess(df):
-    '''Prepare data for modeling
+    '''Prepares a user-item matrix for modeling
 
     1. Create new 'item' column for (Origin City, Destination City) pairs. 
     2. Get user-item count to show number of times a user interacted with item. 
-    3. Remove low-user trips and users with low-items. 
-    4. Split to train-test sets. 
-    5. Convert to sparse matrix for modeling.
+    3. Convert to sparse matrix for modeling.
 
     Returns
     ----------
-    user_item:  
+    user_items: sparse matrix
+        a matrix with rows:users, columns:items. each cell holds the number of interactions.
     '''
     print('\nPreprocessing ... ')
+    df = df[df.acttype.isin(['click','call'])]  # filter only acttype "click" and "call"
     df = _get_od_pairs(df)
-
     user_item_df = _get_user_item_count(df)
-
     user_item_df = _map_cat_to_numeric(user_item_df)
-
-    user_item_mat = _convert_to_sparse_matrix(user_item_df)
-
-    #user_item = _remove_low_freq_data(user_item)
-
-    train, test_idx = _train_test_split(user_item)
-
-    return user_item, train, test_idx
+    user_items = _convert_to_sparse_matrix(user_item_df)
+    return user_items
 
 def _get_od_pairs(df):
     df.dropna(subset=['orgcity','destcity'],inplace=True)
@@ -39,74 +37,121 @@ def _get_od_pairs(df):
     return df
 
 def _get_user_item_count(df):
-    '''Forms a df with number of interactions for each user and items.'''
-    return df.groupby(['userid','item']).size().rename('freq').reset_index()
-    #.unstack()
+    '''Finds number of interactions for each user and items.'''
+    user_item_df = df.groupby(['userid','item']).size().rename('freq').reset_index()
+    return user_item_df
 
 def _map_cat_to_numeric(user_item_df):
-    '''Map "user" and "item" categories to unique numeric values.
+    '''Maps "user" and "item" categories to unique numeric values.
     This is necessary for creating sparse matrix.'''
-    user_item_df['userid'] = user_item_df['userid'].astype('category')
-    user_item_df['item'] = user_item_df['item'].astype('category')
+    user_item_df['userid'] = user_item_df.userid.astype('category')
+    user_item_df['item'] = user_item_df.item.astype('category')
     return user_item_df
 
 def _convert_to_sparse_matrix(user_item_df):
-    return sparse.coo_matrix(user_item_df.freq,
-                            (user_item_df.userid,user_item_df.item))
+    return sparse.coo_matrix((user_item_df.freq,
+                            (user_item_df.userid.cat.codes,
+                                user_item_df.item.cat.codes)))
 
-def _remove_low_freq_data(user_item):
-    '''[LATER- Change this for coo_matrix]'''
-    min_users_per_item = 2
-    user_item = user_item.loc[:,user_item.count()>=min_users_per_item]
+def train_test_split(user_items,pct_test=0.05,split_method='random_sampling'):
+    '''Splits data into train and test sets.
 
-    # Note: data already meets requirement. See print(f'Items per user: {user_item.count(axis=1).agg(["min","mean","median"])}')
-    min_items_per_user = 10
-    user_item = user_item.loc[user_item.count(axis=1)>=min_items_per_user,:]
-    return user_item
-
-def _train_test_split(user_item_mat):
-    '''[Change this for coo_matrix]
-    Split data into train and test sets, using "leave-one-out" approach.
+    Parameters
+    ----------
+    user_items: sparse matrix
+        full user-item data.
+    pct_test: float, optional
+        percentage of data to move to test set.
+    split_method: str, optional
+        options: 'random_sampling' (may add other methods in future)
 
     Returns
     ----------
-    train_mat: 
-        user_item matrix with test values removed. 
-    test_idx:
-        array of indices for test values in original user_item matrix.'''
-    test_idx = np.zeros(len(user_item.shape[0]))    # initialize array
-    test_size = 1
-    train_mat = user_item.copy()
+    user_items_train: sparse matrix
+        user-item data for train set (test values removed). 
+    test_indices: 1d array
+        array of indices for test values in original user_item matrix.
+    '''
+    user_items = user_items.tocsr()
+    if split_method=='random_sampling':
+        # Get list of (user_idx,item_idx) where frequency is non-zero
+        user_item_lst = list(zip(user_items.nonzero()[0],
+                                    user_items.nonzero()[1]))
+        # Get test indices
+        random.seed(0) # for reproducability
+        test_size = int(np.ceil(pct_test*len(user_item_lst)))
+        test_indices = random.sample(user_item_lst,test_size)
 
-    for i,user in user_item.iteritems():
-        item_idx = user.notna().index
-        test_item = np.random.choice(item_idx,test_size)
-        test_idx[i] = test_item
-        train_mat.loc[i,test_item] = np.nan
-    return train_mat, test_idx
+    # Remove test values from train set
+    user_items_train = user_items.copy()
+    test_users, test_items = zip(*test_indices)
+    user_items_train[test_users,test_items] = 0
+    user_items_train.eliminate_zeros()
+    user_items_train.tocoo()
+    return user_items_train, test_users
 
-def validate_CF(model,N=5):
-    '''Measure Recall accuracy using "leave-one-out" approach.
+def validate(model,user_items,user_items_train,test_users):
+    '''Measures accuracy of model on test set.  
 
-    Algorithm
+    Returns
     ----------
-    1. For each user, move one item to test set (no duplicates should remain in train set).
-    2. Get top N recommendations for each user.
-    3. Measure the frequency that the left-out item is captured by top N recommendations.'''
-    pass
+    accuracy: float
+        percentage accuracy of correct recommendations
+    '''
+    scores = []
+    user_items = user_items.tocsr()
+    user_items_train = user_items_train.tocsr()
+
+    for user in test_users:
+        train_items = user_items_train[user,:].toarray().reshape(-1).nonzero()
+        all_items = user_items[user,:].toarray().reshape(-1).nonzero()
+        test_items = np.setdiff1d(all_items,train_items)
+
+        num_test_items = len(test_items)
+        recommendations = model.recommend(user,
+                                        user_items_train,
+                                        N=num_test_items*5)
+
+        matched = np.intersect1d(recommendations,test_items)
+        score = len(matched)/num_test_items
+        scores.append(score)
+
+        '''
+        print(f'Number of train items: {len(train_items[0])}')
+        print(f'Number of test items: {num_test_items}')
+        print(f'Number of recommendations: {len(recommendations)}')
+        print(f'Matched items: {matched}')
+        '''
+
+    accuracy = float(f'{np.mean(scores)*100:.3f}')
+    return accuracy
 
 def main():
-    alpha = 15
-
-    #df = load_from_SQL('dr250_active')
+    # Load Data]
     df = pd.read_csv('data/dr250_active.csv')
 
-    df = preprocess(df)
-    input('Pause')
+    # Prepare Data
+    user_items = preprocess(df)
+    user_items_train, test_users = train_test_split(user_items)
 
-    #model = implicit.als.AlternatingLeastSquares
-    #model.fit(mat)
-    #score = validate_CF(model)
+    '''
+    # Build Model
+    # Model based on paper by Hu et al. "Collaborative Filtering for Implicit Datasets"
+    model = AlternatingLeastSquares(factors=10,
+                                    regularization=0.01,
+                                    iterations=50)
+    confidence=15
+    item_users_train = user_items_train.transpose()     # model fit takes in rows:items, columns:users
+    model.fit(item_users_train*confidence)
+    with open('data/model.pkl','wb') as f:
+        pickle.dump(model,f)
+    '''
+
+    with open('data/model.pkl','rb') as f:
+        model = pickle.load(f)
+    # Validate Model
+    accuracy = validate(model,user_items,user_items_train,test_users)
+    print(f'Accuracy is: {accuracy}%')
 
 if __name__=='__main__':
     main()
